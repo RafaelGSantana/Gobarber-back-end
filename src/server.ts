f102764d8f3e50c494237dadc99a22e9b4ee619a
Rafael Santana <rgs1990419@gmail.com>
2020-04-27 18:58:20 -0300

    Configurando EditorConfig & ESlint

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import express, { response } from "express";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
-});
+app.get("/", (request, response) => response.json({ message: "Hello World" }));
 
 app.listen(3333, () => {
   console.log("Ok");
