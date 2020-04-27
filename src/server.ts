b2a16c68be50b26a83557c4a0854cc8e09bd0e99
Rafael Santana <rgs1990419@gmail.com>
2020-04-27 19:29:32 -0300

    Configurando Importação de arquivos .ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express, { response } from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => response.json({ message: "Hello World" }));
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("Ok");
