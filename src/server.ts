400f192a03d786b41fc64acfd72bbfdded3dbc05
Rafael Santana <rgs1990419@gmail.com>
2020-04-27 19:54:12 -0300

    Configurando o Debugger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import express, { response } from "express";
-import routes from "./routes";
+import express, { response } from 'express';
+import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log("Ok");
+  console.log('Ok');
 });
