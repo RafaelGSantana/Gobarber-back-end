67e07c011b3fa26d8151e2075c9165543fa9c2fa
Rafael Santana <rgs1990419@gmail.com>
2020-05-06 18:18:35 -0300

    Configurando TypeORM

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import express, { response } from 'express';
+import express from 'express';
 import routes from './routes';
+
+import './database';
 
 const app = express();
 
@@ -7,5 +9,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Ok');
+  console.log('Server Started');
 });
