8ba70625ac0aa07efb8e0bb90394c478498eb3a5
Rafael Santana <rgs1990419@gmail.com>
2020-05-10 19:56:18 -0300

    Atualizando avatar - pt 1

---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,45 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+import uploadConfig from '../config/upload';
+import User from '../models/User';
+
+interface Request {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated users can change avatar');
+    }
+
+    // Remove avatar caso exista
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    /**
+     * 'Seta' campo avatar do usuário autenticado (user = user_id) com o avatar
+     * enviado na requisição (avatarFilename) e salva no seu registro.
+     */
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
