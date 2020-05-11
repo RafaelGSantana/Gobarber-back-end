4872b761d367d505c1497c8306f8fa718137c7f6
Rafael Santana <rgs1990419@gmail.com>
2020-05-11 09:57:23 -0300

    Lidando com erros: Global

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,23 +11,19 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const CreateUser = new CreateUserService();
+  const CreateUser = new CreateUserService();
 
-    const user = await CreateUser.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await CreateUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(user);
 });
 
 usersRouter.patch(
@@ -35,20 +31,16 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-      const user = await updateUserAvatar.execute({
-        avatarFilename: request.file.filename,
-        user_id: request.user.id,
-      });
+    const user = await updateUserAvatar.execute({
+      avatarFilename: request.file.filename,
+      user_id: request.user.id,
+    });
 
-      delete user.password;
+    delete user.password;
 
-      return response.json(user);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    return response.json(user);
   },
 );
 
