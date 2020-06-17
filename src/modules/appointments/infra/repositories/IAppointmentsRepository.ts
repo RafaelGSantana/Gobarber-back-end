5782f2bdb3ec1c37a2d86b8bf6052df3a7deee72
Rafael Santana <rgs1990419@gmail.com>
2020-06-17 17:20:17 -0300

    Arquitetura e DDD: Reescrevendo Repositórios

--- src/modules/appointments/infra/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/infra/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,7 @@
-/** Métodos que o repositório Appointments precisa ter */
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointment';
 
 export default interface IApppointmentsRepository {
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,17 +1,34 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import IAppointmentsRepository from '@modules/appointments/infra/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointment';
+
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
-@EntityRepository(Appointment)
-class AppointmentRepository extends Repository<Appointment>
-  implements IAppointmentsRepository {
+class AppointmentRepository implements IAppointmentsRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = await this.findOne({
+    const findAppointment = await this.ormRepository.findOne({
       where: { date },
     });
 
     return findAppointment;
+  }
+
+  public async create({
+    date,
+    provider_id,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = this.ormRepository.create({ provider_id, date });
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
   }
 }
 
