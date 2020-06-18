ee4a7594a5dcc1790fd6cfe5a36b696db9ea9889
Rafael Santana <rgs1990419@gmail.com>
2020-06-18 18:57:29 -0300

    Testes e TDD: Criando primeiro teste

---
+++ src/modules/appointments/repositories/fakes/FakeApointmentsRepository.ts
@@ -0,0 +1,36 @@
+import { uuid } from 'uuidv4';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointment';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointmentByDate = this.appointments.find(appointment => {
+      appointment.date === date;
+    });
+
+    return findAppointmentByDate;
+  }
+
+  public async create({
+    date,
+    provider_id,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), date, provider_id });
+    // A linha acima substitui as 3 abaixo
+    // appointment.id = uuid();
+    // appointment.date = date;
+    // appointment.provider_id = provider_id;
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
