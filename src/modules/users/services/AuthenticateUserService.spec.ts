5662df84732fea80861211736ea988f21ac6e842
Rafael Santana <rgs1990419@gmail.com>
2020-06-19 11:32:13 -0300

    Testes e TDD: Teste de Autenticação I

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,13 +1,20 @@
 import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
+import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 import CreateUserService from '@modules/users/services/CreateUserService';
 
-describe('CreateUser', () => {
-  it('should be able to create a new appointment', async () => {
+describe('AuthenticateUser', () => {
+  it('should be able to authenticate', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
     const fakeHashProvider = new FakeHashProvider();
+
     const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const authenticateUser = new AuthenticateUserService(
       fakeUsersRepository,
       fakeHashProvider,
     );
@@ -18,29 +25,12 @@ describe('CreateUser', () => {
       password: '123123',
     });
 
-    expect(user).toHaveProperty('id');
-  });
-
-  it('it should not be able to create a new user with same email from another', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-    const createUser = new CreateUserService(
-      fakeUsersRepository,
-      fakeHashProvider,
-    );
-
-    await createUser.execute({
-      name: 'John Doe',
+    const response = await authenticateUser.execute({
       email: 'johndoe@email.com',
       password: '123123',
     });
 
-    expect(
-      createUser.execute({
-        name: 'John Doe',
-        email: 'johndoe@email.com',
-        password: '123123',
-      }),
-    ).rejects.toBeInstanceOf(AppError);
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
   });
 });
--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,9 +1,9 @@
-import { hash } from 'bcryptjs';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import User from '@modules/users/infra/typeorm/entities/User';
-import IUsersRepository from '../repositories/IUsersRepository';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
   name: string;
@@ -16,6 +16,9 @@ class CreateUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -25,7 +28,7 @@ class CreateUserService {
       throw new AppError('Email address already used.');
     }
 
-    const hashedPassword = await hash(password, 8);
+    const hashedPassword = await this.hashProvider.generateHash(password);
 
     const user = await this.usersRepository.create({
       name,
