346b34cae795cee46b0e0304f2dcb8a8a542d1fc
Rafael Santana <rgs1990419@gmail.com>
2020-06-19 10:17:55 -0300

    Testes e TDD: Teste de criação de usuário

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,37 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import CreateUserService from '@modules/users/services/CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123123',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('it should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123123',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'John Doe',
+        email: 'johndoe@email.com',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
