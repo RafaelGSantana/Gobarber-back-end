9a4522a18abd811fa245593a290b41389e18ce0a
Rafael Santana <rgs1990419@gmail.com>
2020-06-21 13:23:05 -0300

    Perfil do usuário: Aplicando TDD na prática

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,30 @@
+import { injectable, inject } from 'tsyringe';
+
+// import AppError from '@shared/errors/AppError';
+// import User from '@modules/users/infra/typeorm/entities/User';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido',
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
