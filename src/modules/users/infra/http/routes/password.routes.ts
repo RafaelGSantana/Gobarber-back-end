7d6c8d84fd992a6eb69f190c047b7006eea2f4bb
Rafael Santana <rgs1990419@gmail.com>
2020-06-22 14:41:46 -0300

    Salvando tokens no banco de dados

---
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+import ForgotPasswordController from '@modules/users/infra/http/controllers/ForgotPasswordController';
+import ResetPasswordController from '@modules/users/infra/http/controllers/ResetPasswordController';
+
+const passwordRouter = Router();
+const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
+
+passwordRouter.post('/', forgotPasswordController.create);
+passwordRouter.post('/', resetPasswordController.create);
+
+export default passwordRouter;
