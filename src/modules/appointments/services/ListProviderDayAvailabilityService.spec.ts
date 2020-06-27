dc6cea3cf4817bb25b4f1d55c46465efdf9205a3
Rafael Santana <rgs1990419@gmail.com>
2020-06-27 18:21:12 -0300

    Excluindo horários antigos

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -15,12 +15,16 @@ describe('ListProviders', () => {
   it('should be able to list the providers day availability', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 5, 28, 8, 0, 0),
+      date: new Date(2020, 5, 28, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 5, 28, 9, 0, 0),
+      date: new Date(2020, 5, 28, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 5, 28, 11).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
@@ -34,8 +38,13 @@ describe('ListProviders', () => {
       expect.arrayContaining([
         { hour: 8, available: false },
         { hour: 9, available: false },
-        { hour: 10, available: true },
-        { hour: 11, available: true },
+        { hour: 10, available: false },
+        { hour: 12, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
+        { hour: 17, available: true },
       ]),
     );
   });
