60bf19b817ee88e637b52da2b961ff2646190849
Rafael Santana <rgs1990419@gmail.com>
2020-06-28 11:11:01 -0300

    Rotas e controllers: disponibilidade prestador

---
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+
+export default class ProvidersDayAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { day, month, year } = request.body;
+
+    const listProviderDayAvailability = container.resolve(
+      ListProviderDayAvailabilityService,
+    );
+
+    const availability = await listProviderDayAvailability.execute({
+      provider_id,
+      month,
+      day,
+      year,
+    });
+
+    return response.json(availability);
+  }
+}
