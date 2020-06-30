f63add83ea9ac252c1ac855017d7dfad5a9600e4
Rafael Santana <rgs1990419@gmail.com>
2020-06-30 10:33:21 -0300

    Upload de arquivos para o Amazon S3

--- src/shared/container/providers/StorageProviders/index.ts
+++ src/shared/container/providers/StorageProviders/index.ts
@@ -1,14 +1,17 @@
 import { container } from 'tsyringe';
+import uploadConfig from '@config/upload';
 
 import IStorageProvider from '@shared/container/providers/StorageProviders/models/IStorageProvider';
 
 import DiskStorageProvider from '@shared/container/providers/StorageProviders/implementations/DiskStorageProvider';
+import S3StorageProvider from '@shared/container/providers/StorageProviders/implementations/S3StorageProvider';
 
 const providers = {
   disk: DiskStorageProvider,
+  s3: S3StorageProvider,
 };
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  providers.disk,
+  providers[uploadConfig.driver],
 );
