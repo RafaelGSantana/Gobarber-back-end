46f3b65ab969486e56f92e4174099d4614bd7a61
Rafael Santana <rgs1990419@gmail.com>
2020-07-01 08:38:59 -0300

    Express rate limit

---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from 'express';
+import redis from 'redis';
+import AppError from '@shared/errors/AppError';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASS || undefined,
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'rateLimit',
+  points: 5,
+  duration: 5,
+  blockDuration: 30,
+});
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (err) {
+    throw new AppError(
+      'Too many requests. You must be wait 30 seconds for the next request',
+      429,
+    );
+  }
+}
